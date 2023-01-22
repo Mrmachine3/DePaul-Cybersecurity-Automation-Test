f1029b9f123b76d0843689e0ebf92c25c3287705
mrmachine <atone36@gmail.com>
2023-01-21 19:50:43 -0600

    Add hello-world program

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author: Anthony M. Rodriguez
+# Date: 1/24/2023
+# Path: /home/mrmachine/DePaul-Cybersecurity-Automation-Test/hello-world.py
+# Usage: Invoke this program to print out hello world
+# Description: This program is typically used to verify the correct installation of the desired Python version by printing text, commonly the string "Hello, World!"
+
+def hello():
+    print(f'Hello, World!')
+
+
+hello()
